a48546af18beecd47fc2c30af9e0c9f596bf4a4a
Isha Kashif <ishak@udel.edu>
2025-03-06 23:47:20 -0500

    Finished Reveal Answer

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,30 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
+// import { ChangeType } from "./components/ChangeType";
+import { RevealAnswer } from "./components/RevealAnswer";
+// import { StartAttempt } from "./components/StartAttempt";
+// import { TwoDice } from "./components/TwoDice";
+// import { CycleHoliday } from "./components/CycleHoliday";
+ import { Counter } from "./components/Counter";
 
 function App(): React.JSX.Element {
-=======
-import { ChangeType } from "./components/ChangeType";
-import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
-import { Counter } from "./components/Counter";
-
-function App(): JSX.Element {
->>>>>>> origin/task-state
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
             <RevealAnswer></RevealAnswer>
             <hr />
-            <StartAttempt></StartAttempt>
+            {/* <StartAttempt></StartAttempt> */}
             <hr />
-            <TwoDice></TwoDice>
+            {/* <TwoDice></TwoDice> */}
             <hr />
-            <ChangeType></ChangeType>
+            {/* <ChangeType></ChangeType> */}
             <hr />
-            <CycleHoliday></CycleHoliday>
->>>>>>> origin/task-state
+            {/* <CycleHoliday></CycleHoliday> */}
         </div>
     );
 }
